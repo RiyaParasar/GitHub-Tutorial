10c048d302ce595efc2e276a086ca124cee7c2be
RiyaParasar <79544307+RiyaParasar@users.noreply.github.com>
2021-09-02 11:06:50 -0700

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void swapping(int &a, int &b) {         //swap the content of a and b
+   int temp;
+   temp = a;
+   a = b;
+   b = temp;
+}
+void display(int *array, int size) {
+   for(int i = 0; i<size; i++)
+      cout << array[i] << " ";
+   cout << endl;
+}
